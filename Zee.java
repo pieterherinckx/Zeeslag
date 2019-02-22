9c79df193712e3008159f6400cfb93c0d17608b7
pieterherinckx <pieterherinckx@atheneumtienen.be>
2019-02-22 14:14:35 +0100

    Update Zee.java

--- Zee.java
+++ Zee.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.Random;
 /**
  * Write a description of class Zee here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Zee
@@ -13,40 +13,40 @@ public class Zee
     private ArrayList<Boot> botenlijst;
 
     public Zee(int afmetingX, int afmetingY){
-        this.afmetingX =afmetingX;
+        this.afmetingX = afmetingX;
         this.afmetingY = afmetingY;
-        this.botenlijst = new ArrayList <Boot>();
+        this.botenlijst = new ArrayList<Boot>();
     }
 
     public Zee(){
         this(10,10);
     }
-    
-    public void voegBootToe(Boot gegevenBoot){
-        if(!overlaptBoot(gegevenBoot)){
-            botenlijst.add(gegevenBoot);
+
+    public void voegBootToe(Boot nieuweBoot){
+        if(!overlaptBoot(nieuweBoot)){
+            botenlijst.add(nieuweBoot);
         }
-        else{
-            System.out.println("De boot kan niet toegevoegd worden aangezien er een overlapping plaats vindt.");
+        else {
+            System.out.println("De boot kan niet toegevoegd worden omdat er een overlapping met een reeds bestaande boot zich voordoet.");
         }
     }
-    
+
     public void toonBoten(){
         for(Boot boot: botenlijst){
             boot.toonBoot();
-        }   
+        }
     }
-    
+
     public boolean isRaak(int raakX, int raakY){
         boolean raak = false;
         for(Boot boot: botenlijst){
-            if(boot.isRaak(raakX,raakY)){
+            if(boot.isRaak(raakX, raakY)){
                 raak = true;
             }
         }
         return raak;
     }
-    
+
     public boolean overlaptBoot(Boot testboot){
         boolean overlapt = false;
         for(Boot boot: botenlijst){
@@ -60,18 +60,18 @@ public class Zee
     public void maakWillekeurigeBoot(){
         Random rg = new Random();
         boolean horizontaal = rg.nextBoolean();
-        int grootte = rg.nextInt(3) + 2;
-        int x;
-        int y;
-        if (horizontaal){
-            x = rg.nextInt(afmetingX - grootte + 1);
-            y = rg.nextInt(afmetingY);
+        int grootte = rg.nextInt(4) + 2;
+        int positieX;
+        int positieY;
+        if(horizontaal){
+            positieX = rg.nextInt(afmetingX - grootte + 1) + 1;
+            positieY = rg.nextInt(afmetingY) + 1;
         }
-        else{
-            x = rg.nextInt(afmetingX);
-            y = rg.nextInt(afmetingY - grootte + 1);        
+        else {
+            positieX = rg.nextInt(afmetingX) + 1;
+            positieY = rg.nextInt(afmetingY - grootte + 1) + 1;
         }
-        Boot boot = new Boot(x,y,grootte,horizontaal);
+        Boot boot = new Boot(positieX, positieY, grootte, horizontaal);
         voegBootToe(boot);
     }
 }
