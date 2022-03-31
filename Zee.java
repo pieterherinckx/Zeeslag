e3f388d8cf061ad8f4f8e52efd5f8cb34307c74b
pieterherinckx <pieterherinckx@atheneumtienen.be>
2022-03-31 16:07:23 +0200

    Update Zee.java

--- Zee.java
+++ Zee.java
@@ -1,78 +1,77 @@
 import java.util.ArrayList;
 import java.util.Random;
+
 /**
-* class Zee - geef hier een beschrijving van deze class
-*
-* @author (jouw naam)
-* @version (versie nummer of datum)
-*/
+ * class Zee - geef hier een beschrijving van deze class
+ *
+ * @author (jouw naam)
+ * @version (versie nummer of datum)
+ */
 public class Zee
 {
-private int afmetingX;
-private int afmetingY;
-private ArrayList<Boot> botenlijst;
+    private int afmetingX;
+    private int afmetingY;
+    private ArrayList<Boot> lijst;
 
-public Zee(int afmetingX, int afmetingY){
-    this.afmetingX = afmetingX;
-    this.afmetingY = afmetingY;
-    this.botenlijst = new ArrayList<Boot>();
-}
-
-public Zee(){
-    this(10,10);
-}
-
-public void voegBootToe(Boot nieuweBoot){
-    if(!overlaptBoot(nieuweBoot)){
-        botenlijst.add(nieuweBoot);
+    public Zee(int afmetingX, int afmetingY){
+        this.afmetingX = afmetingX;
+        this.afmetingY = afmetingY;
+        lijst = new ArrayList<>();
     }
-    else{
-        System.out.println("De boot kan niet toegevoegd worden wegens overlapping");
-    }
-}
 
-public void toonBoten(){
-    for (Boot boot: botenlijst){
-        boot.toonBoot();
+    public Zee(){
+        this(10,10);
     }
-}
 
-public boolean isRaak(int raakX, int raakY){
-    boolean raak = false;
-    for (Boot testboot: botenlijst){
-        if(testboot.isRaak(raakX, raakY)){
-            raak = true;
+    public void voegBootToe(Boot nieuweBoot){
+        if(!overlaptBoot(nieuweBoot)){
+            lijst.add(nieuweBoot);
+        }
+        else{
+            System.out.println("De boot kan niet toegevoegd worden wegens overlapping");
         }
     }
-    return raak;
-}
 
-public boolean overlaptBoot(Boot nieuweBoot){
-    boolean overlapt = false;
-    for (Boot testboot: botenlijst){
-        if(testboot.overlaptBoot(nieuweBoot)){
-            overlapt = true;
+    public void toonBoten(){
+        for(Boot testboot: lijst){
+            testboot.toonBoot();
         }
-    }        
-    return overlapt;
-}
+    }
 
-public void maakWillekeurigeBoot(){
+    public boolean isRaak(int raakX, int raakY){
+        boolean raak = false;
+        for(Boot testboot: lijst){
+            if(testboot.isRaak(raakX, raakY)){
+                raak = true;
+            }
+        }
+        return raak;
+    }
+
+    public boolean overlaptBoot(Boot nieuweBoot){
+        boolean overlapt = false;
+        for(Boot testboot: lijst){
+            if(testboot.overlaptBoot(nieuweBoot)){
+                overlapt = true;
+            }
+        }
+        return overlapt;
+    }
+
+    public void maakWillekeurigeBoot(){
         Random random = new Random();
+        int grootte = random.nextInt(4)+2;
+        boolean horizontaal = random.nextBoolean();
         int x;
         int y;
-        int grootte = random.nextInt(5) + 2; 
-        boolean horizontaal = random.nextBoolean();        
-
         if(horizontaal){
-            x = random.nextInt(afmetingX - grootte + 1) + 1;
-            y = random.nextInt(afmetingY) + 1;
+            x = random.nextInt(afmetingX - grootte + 1);
+            y = random.nextInt(afmetingY);
         }
-        else {
-            x = random.nextInt(afmetingX) + 1;
-            y = random.nextInt(afmetingY - grootte + 1) + 1;
+        else{
+            x = random.nextInt(afmetingX);
+            y = random.nextInt(afmetingY - grootte + 1);
         }
-  
         Boot nieuweBoot = new Boot(x, y, grootte, horizontaal);
         voegBootToe(nieuweBoot);
     }
